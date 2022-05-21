00a69b60688796423d415c0492c5ae92e7f0d9c9
KKPROGRAMING <93694492+KKPROGRAMING@users.noreply.github.com>
2022-05-21 16:46:09 +0800

    Update test_write.c

--- ipc/pipe/test_write.c
+++ ipc/pipe/test_write.c
@@ -15,33 +15,30 @@ int main(){
 
     //写内容进入管道，直到留下2*4096空间
     for(int i=1 ; i<=14 ; i++)
-        write(fildes[1],"helloworld",SIZE);
+        write(filedes[1],"helloworld",SIZE);
 
     //父进程写，子进程读
     if(fork()==0){
         char buff[SIZE];
         close(filedes[1]);
 
-        printf("(Child)sleeping......\n");
+        printf("(Child 1)sleeping......\n");
         usleep(50000);
 
-        printf("(Child)awake!\n");
+        printf("(Child 2)awake!\n");
         read(filedes[0],buff,SIZE);
-        printf("(Child)read %d Bytes.\n");
+        printf("(Child)read %d Bytes.\n",SIZE);
 
-        printf("(Child)sleeping......\n");
-        usleep(50000);
-
-        printf("(Child)awake!\n");
         //分两次读出，每次读出4096B
+        //no sleep!否则写进程会认为没有读进程而退出
         read(filedes[0],buff,SIZE);
-        printf("(Child)read %d Bytes.\n");
+        printf("(Child 4)read %d Bytes.\n",SIZE);
         read(filedes[0],buff,SIZE);
-        printf("(Child)read %d Bytes.\n");
+        printf("(Child 5)read %d Bytes.\n",SIZE);
 
         //杀死读进程
-        close(fildes[0]);
-        printf("(Child)killed!");
+        close(filedes[0]);
+        printf("(Child 6)killed!\n");
         return 0 ;
 
     }
@@ -52,26 +49,28 @@ int main(){
         usleep(500);
         //u>n,u=2*4096,n=4096
         ret = write(filedes[1],"helloworld",SIZE);
-        printf("(Parent)wrote %d, return %d.\n",SIZE,ret);
+        printf("(Parent 1)wrote %d, return %d.\n",SIZE,ret);
         //u=n,u=4096,n=4096
         ret = write(filedes[1],"helloworld",SIZE);
-        printf("(Parent)wrote %d, return %d.\n",SIZE,ret);
+        printf("(Parent 2)wrote %d, return %d.\n",SIZE,ret);
 
         //u<n<4096,u=0,n=4096
         //等待，直到有n-u=4096字节被读出，写入n字节并返回n
         ret = write(filedes[1],"helloworld",SIZE);
-        printf("(Parent)wrote %d, return %d.\n",SIZE,ret);
+        printf("(Parent 3)wrote %d, return %d.\n",SIZE,ret);
+        //usleep(200);
 
-        //u<n,n>4096(n=4096+4)
+        //u<n,u=0,n>4096(n=4096+4)
         ret = write(filedes[1],"helloworld",SIZE+4);
-        printf("(Parent)wrote %d, return %d.\n",SIZE+4,ret);
+        printf("(Parent 4)wrote %d, return %d.\n",SIZE+4,ret);
         usleep(200);
 
         //没有读进程，u>0，尝试写入n=1
-        //预期结果是写入失败，返回-EPIPE
+        //预期结果是写入失败，进程返回-EPIPE
         ret = write(filedes[1],"t",1);
-        printf("(Parent)wrote %d, return %d.\n",1,ret);
+        printf("(Parent 5)wrote %d, return %d.\n",1,ret);
 
+        close(filedes[1]);
         return 0;      
     }
 }
