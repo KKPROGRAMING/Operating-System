51b8350ab5697c03490c8be97652fb748cbc2996
kk <93694492+KKPROGRAMING@users.noreply.github.com>
2022-05-27 10:16:52 +0800

    Update test_read.c

--- ipc/pipe/test_read.c
+++ ipc/pipe/test_read.c
@@ -1,3 +1,4 @@
+
 // pipe test
 #include <unistd.h>
 #include <semaphore.h>
@@ -11,9 +12,9 @@
 
 #define SIZE 32
 
-sem_t *mutex;
-char *mutex_name = "mutex";
-int mutex_value = 1;
+sem_t *awake;
+char *awake_name = "awake";
+int awake_value = 0;
 
 int main()
 {
@@ -23,7 +24,7 @@ int main()
         return -1;
 
     //初始化有名信号量
-    mutex = sem_open(mutex_name, O_CREAT, 0644, mutex_value);
+    awake = sem_open(awake_name, O_CREAT, 0644, awake_value);
 
     //父进程读，子进程写
     //关闭父进程写管道，关闭子进程读管道
@@ -31,21 +32,19 @@ int main()
     {
         close(filedes[0]);
 
-        //有睡眠写进程
-        sem_wait(mutex);
+        //无睡眠写进程
         write(filedes[1], "helloworld", 10);
         printf("(Child)wrote 10. >>>helloworld\n");
-        printf("(Child)sleeping......\n");
-        sem_post(mutex);
-        usleep(1000000);
 
+        //有睡眠写进程
+        sem_wait(awake);
 
-        //无睡眠写进程
-        printf("(Child)awake!\n");
         //执行下面的写操作时，p=0，读进程等待写进程写数据，然后再读取数据
         write(filedes[1], "bonjourworld", 12);
         printf("(Child)wrote 12. >>>bonjourworld\n");
-        usleep(500);
+
+        write(filedes[1], "ciaoworld", 9);
+        printf("(Child)wrote 9. >>>ciaoworld\n");
 
         //最后写入一次数据，关闭写进程
         write(filedes[1], "goodbyeworld", 12);
@@ -60,53 +59,56 @@ int main()
         close(filedes[1]);
         char buff[SIZE];
 
-        //先等待写进程写入一些东西
+        //先等待写进程写入一些内容
         usleep(500);
 
-        //有睡眠写进程
-        sem_wait(mutex);
+        //无睡眠写进程
         // p!=0,p>n
-        memset(buff,0,SIZE);
+        memset(buff, 0, SIZE);
         ret = read(filedes[0], buff, 2);
         printf("(Parent)read 2, return %d, the content is:%s\n", ret, buff);
-        // p!=0,0<p<n
-        memset(buff,0,SIZE);
+
+        // p!=0,0<p<n，只能读出p=8
+        memset(buff, 0, SIZE);
         ret = read(filedes[0], buff, 12);
         printf("(Parent)read 12, return %d, the content is:%s\n", ret, buff);
-        // p=0,waiting(有睡眠写) -> p=0,waiting(无睡眠写) -> p!=0,p>n(无睡眠写)
-        //读进程等待写进程写数据，然后再读取数据
-        memset(buff,0,SIZE);
+
+        //有睡眠写，应先等待写进程写入12
+        sem_post(awake);
+
+        //p=0,n=2,读进程等待写进程写数据，然后再读取数据
+        memset(buff, 0, SIZE);
         ret = read(filedes[0], buff, 2);
         printf("(Parent)read 2, return %d, the content is:%s\n", ret, buff);
-        sem_post(mutex);
+        //读完后p剩余10
 
-        //无睡眠写进程
-        // p!=0,0<p<n，理想情况返回0，p变为0，读进程不等待
-        ret = read(filedes[0], buff, 24);
-        printf("(Parent)read 24, return %d, the content is:%s\n", ret, buff);
+        //p=10,n=15,0<p<n，预想情况应先等待p写入9再读出15
+        memset(buff, 0, SIZE);
+        ret = read(filedes[0], buff, 15);
+        printf("(Parent)read 15, return %d, the content is:%s\n", ret, buff);
 
         //等待写进程最后一次写入数据，没有写进程
         usleep(500);
         // p!=0,p>n
-        memset(buff,0,SIZE);
+        memset(buff, 0, SIZE);
         ret = read(filedes[0], buff, 2);
         printf("(Parent)read 2, return %d, the content is:%s\n", ret, buff);
         // 0<p<n
-        memset(buff,0,SIZE);
+        memset(buff, 0, SIZE);
         ret = read(filedes[0], buff, 16);
         printf("(Parent)read 16, return %d, the content is:%s\n", ret, buff);
         // p=0
-        memset(buff,0,SIZE);
+        memset(buff, 0, SIZE);
         ret = read(filedes[0], buff, 2);
         printf("(Parent)read 2, return %d, the content is:%s\n", ret, buff);
     }
 
     //关闭信号量
-    if (sem_close(mutex) == -1)
+    if (sem_close(awake) == -1)
         return -1;
 
     //删除信号量
-    if (sem_unlink(mutex_name) == -1)
+    if (sem_unlink(awake_name) == -1)
         return -1;
     return 0;
 }
